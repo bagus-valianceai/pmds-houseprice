12578f0ba501dc0d1320a1dbc0e2d0f15d0e7635
cbagusjk <cbagusjk@gmail.com>
2022-09-13 12:39:51 +0700

    update api

--- src/api.py
+++ src/api.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Hello PMDS!</h1>"
 
 def main():
     app.run(host = '0.0.0.0', port = 8080)
